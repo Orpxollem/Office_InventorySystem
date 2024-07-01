0147f410e974df46f83ea8bcae92448181bfe625
Orpxollem <mkkking@st.ug.edu.gh>
2024-07-01 04:27:12 +0000

    Updated and excluded some existing files

--- office_inventorysystem/app.py
+++ office_inventorysystem/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 from flask_cors import CORS
 
 app = Flask(__name__)
--- office_inventorysystem/venv/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from app import app
-from venv.models import Staff
-
-@app.route("/signin")
-def signin():
-    return Staff().signin()
