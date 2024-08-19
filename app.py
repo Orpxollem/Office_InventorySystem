432e88113723f0f551dcf55adcd7bb7e193388c4
Orpxollem <mkkking@st.ug.edu.gh>
2024-08-19 00:36:54 +0000

    Updated API for the database ssection

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def signin():
         if user:
             return jsonify({'success': True, 'message': 'Login successful', 'userId': str(user['_id'])})
         else:
-            return jsonify({'success': False, 'message': 'Invalid credentials'}), 401
+            return jsonify({'success': False, 'message': 'Invalid credentials'})
         
 
 @app.route('/user/<id>', methods=['GET'])
@@ -41,7 +41,7 @@ def get_user(id):
     if user:
         return jsonify({'name': user['name'], 'staffId': user['staffId'], 'position': user['position'], 'email': user['email'], 'password': user['password']})
     else:
-        return jsonify({'error': 'User not found'}), 404
+        return jsonify({'error': 'User not found'})
     
 @app.route('/update_password', methods=['POST'])
 @cross_origin()
@@ -67,7 +67,7 @@ def update_password():
             else:
                     return jsonify({'success': False, 'message': 'Current password is incorrect!'})
         else:
-            return jsonify({'success': False, 'message': 'Password update unsuccessful'}), 404
+            return jsonify({'success': False, 'message': 'Password update unsuccessful'})
 
 @app.route('/add_user', methods=['POST'])
 @cross_origin()
@@ -82,7 +82,7 @@ def add_user():
         employee = db['Employees'].find_one({'emplName': employeeName, 'emplID': employeeID, 'email': email, 'contact': phoneNumber})
         
         if employee:
-            return jsonify({'success': False, 'message': 'User already exists'}), 409
+            return jsonify({'success': False, 'message': 'User already exists'})
         else:
             db['Employees'].insert_one({'emplName': employeeName, 'emplID': employeeID, 'email': email, 'contact': phoneNumber})
             return jsonify({'success': True, 'message': 'User added successfully'})
@@ -104,7 +104,7 @@ def update_user():
             db['Employees'].update_one({'emplID': employeeID}, {'$set': {'emplName': employeeName, 'email': email, 'contact': phoneNumber}})
             return jsonify({'success': True, 'message': 'User updated successfully'})
         else:
-            return jsonify({'success': False, 'message': 'User does not exist'}), 404
+            return jsonify({'success': False, 'message': 'User does not exist'})
         
 @app.route('/remove_user', methods=['POST'])
 @cross_origin()
@@ -120,7 +120,7 @@ def remove_user():
             db['Employees'].delete_one({'emplName': employeeName, 'emplID': employeeID})
             return jsonify({'success': True, 'message': 'User removed successfully'})
         else:
-            return jsonify({'success': False, 'message': 'User does not exist'}), 404
+            return jsonify({'success': False, 'message': 'User does not exist'})
 
 
 @app.route('/employees', methods=['GET'])
@@ -147,6 +147,101 @@ def get_report():
     for report in reports:
         report['_id'] = str(report['_id'])
     return jsonify(reports)
+
+
+@app.route('/add_inventory', methods=['POST'])
+@cross_origin()
+def add_inventory():
+    if request.method == 'POST':
+        data = request.json
+        equipmentID = data.get('equipmentID')
+        equipmentName = data.get('equipmentName')
+        type = data.get('type')
+        condition = data.get('condition')
+        location = data.get('location')
+        assignment = data.get('assignment')
+        purchasedate = data.get('purchasedate')
+
+    inventory = db['Inventory'].find_one({'equipment_id': equipmentID, 'name': equipmentName, 'type': type, 'condition': condition, 'location': location, 'assigned_to': assignment, 'purchase_date': purchasedate})
+
+    if inventory:
+        return jsonify({'success': False, 'message': 'Item already exists'})
+    else:
+        db['Inventory'].insert_one({'equipment_id': equipmentID, 'name': equipmentName, 'type': type, 'condition': condition, 'location': location, 'assigned_to': assignment, 'purchase_date': purchasedate})
+        return jsonify({'success': True, 'message': 'Item added successfully'})
+
+
+@app.route('/update_inventory', methods=['POST'])
+@cross_origin()
+def update_inventory():
+    if request.method == 'POST':
+        data = request.json
+        equipmentID = data.get('equipmentID')
+        condition = data.get('condition')
+        location = data.get('location')
+        assignment = data.get('assignment')
+
+    inventory = db['Inventory'].find_one({'equipment_id': equipmentID})
+
+    if inventory:
+        db['Inventory'].update_one({'equipment_id': equipmentID}, {'$set': {'condition': condition, 'location': location, 'assigned_to': assignment}})
+        return jsonify({'success': True, 'message': 'Item updated successfully'})
+    else:
+        return jsonify({'success': False, 'message': 'Item doesnot exist'})
+    
+@app.route('/remove_inventory', methods=['POST'])
+@cross_origin()
+def remove_inventory():
+    if request.method == 'POST':
+        data = request.json
+        equipmentID = data.get('equipmentID')
+        equipmentName = data.get('equipmentName')
+
+    inventory = db['Inventory'].find_one({'equipment_id': equipmentID, 'name': equipmentName})
+
+    if inventory:
+        db['Inventory'].delete_one({'equipment_id': equipmentID, 'name': equipmentName})
+        return jsonify({'success': True, 'message': 'Item removed successfully'})
+    else:
+        return jsonify({'success': False, 'message': 'Item does not exist'})
+    
+@app.route('/add_report', methods=['POST'])
+@cross_origin()
+def add_report():
+    if request.method == 'POST':
+        data = request.json
+        assignmentID = data.get('assignmentID')
+        employeeID = data.get('employeeID')
+        equipmentID = data.get('equipmentID')
+        assignmentDate = data.get('assignmentDate')
+        date = data.get('date')
+
+    report = db['Reports'].find_one({'assignment_id': assignmentID})
+    
+    if report:
+        return jsonify({'success': False, 'message': 'Report already exists'})
+    else:
+        db['Reports'].insert_one({'assignment_id': assignmentID, 'employee_id': employeeID, 'equipment_id': equipmentID, 'assignment_date': assignmentDate, 'return_date': date})
+        return jsonify({'success': True, 'message': 'Report added successfully'})
+    
+@app.route('/update_report', methods=['POST'])
+@cross_origin()
+def update_report():
+    if request.method == 'POST':
+        data = request.json
+        assignmentID = data.get('assignmentID')
+        employeeID = data.get('employeeID')
+        equipmentID = data.get('equipmentID')
+        assignmentDate = data.get('assignmentDate')
+        return_date = data.get('returnDate')
+
+    report = db['Reports'].find_one({'assignment_id': assignmentID})
+
+    if report:
+        db['Reports'].update_one({'assignment_id': assignmentID}, {'$set': {'employee_id': employeeID, 'equipment_id': equipmentID, 'assignment_date': assignmentDate, 'return_date': return_date}})
+        return jsonify({'success': True, 'message': 'Report updated successfully'})
+    else:
+        return jsonify({'success': False, 'message': 'Report does not exist'})
 
 
 if __name__ == "__main__":
--- office_inventorysystem/src/pages/Database.js
+++ office_inventorysystem/src/pages/Database.js
@@ -9,9 +9,53 @@ import { MdOutlineAddchart } from "react-icons/md";
 
 
 const AddToInventory = () => {
+
+    const handleAddToInventory = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const formData = {
+            equipmentID: form.equipmentID.value,
+            equipmentName: form.equipmentName.value,
+            type: form.type.value,
+            condition: form.condition.value,
+            location: form.location.value,
+            assignment: form.assignment.value,
+            purchasedate: form.purchasedate.value,
+        };
+
+        fetch('http://localhost:5000/add_inventory', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Item Add Failed!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                alert('Item added successfully!');
+                form.reset();
+            } else {
+                alert(`Item add failed: ${data.message}`);
+                form.reset();
+            }
+        })
+        .catch((error) => {
+            alert(`Item add failed: ${error.message}`);
+            form.reset();
+        });
+    }
+
     return(
         <div className='DatabaseFormContainer'>
-            <form name='additem'>
+            <form name='additem' onSubmit={handleAddToInventory}>
                 <div>
                     <label>Add Item To Inventory</label>
                     <input type="text" name="equipmentID" placeholder='Equipment ID' required/>
@@ -31,9 +75,48 @@ const AddToInventory = () => {
 }
 
 const RemoveItem = () => {
+
+    const handleRemoveItem = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const formData = {
+            equipmentID: form.equipmentID.value,
+            equipmentName: form.equipmentName.value,
+        };
+
+        fetch('http://localhost:5000/remove_inventory', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Inventory Item Removal Failed!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                alert('Inventory Item removed successfully!');
+                form.reset();
+            } else {
+                alert(`Inventory Item removal failed: ${data.message}`);
+                form.reset();
+            }
+        })
+        .catch((error) => {
+            alert(`Inventory Item removal failed: ${error.message}`);
+            form.reset();
+        });
+    }
+
     return(
         <div className='DatabaseFormContainer'>
-            <form name='removeitem'>
+            <form name='removeitem' onSubmit={handleRemoveItem}>
                 <div>
                     <label>Remove Item From Inventory</label>
                     <input type="text" name="equipmentID" placeholder='Equipment ID' required/>
@@ -48,9 +131,50 @@ const RemoveItem = () => {
 }
 
 const UpdateItem = () => {
+
+    const handleUpdateItem = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const formData = {
+            equipmentID: form.equipmentID.value,
+            condition: form.condition.value,
+            location: form.location.value,
+            assignment: form.assignment.value,
+        };
+
+        fetch('http://localhost:5000/update_inventory', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Update Inventory Item Failed!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                alert('Inventory Item updated successfully!');
+                form.reset();
+            } else {
+                alert(`Inventory Item update failed: ${data.message}`);
+                form.reset();
+            }
+        })
+        .catch((error) => {
+            alert(`Inventory Item update failed: ${error.message}`);
+            form.reset();
+        });
+    }
+
     return(
         <div className='DatabaseFormContainer'>
-        <form name='updateitem'>
+        <form name='updateitem'onSubmit={handleUpdateItem}>
             <div>
                 <h2>Update Inventory Item</h2>
                 <label>ID For Item Update</label>
@@ -58,7 +182,7 @@ const UpdateItem = () => {
                 <label>Update Details</label>
                 <input type="text" name="condition" placeholder='Condition' required/>
                 <input type="text" name="location" placeholder='Location' required/>
-                <input type="text" name="Assignment" placeholder='Assignment' required/>
+                <input type="text" name="assignment" placeholder='Assignment' required/>
             </div>
             <div className='form-button'>
                 <button type="submit" className='update-button'>Update Item</button>
@@ -69,9 +193,51 @@ const UpdateItem = () => {
 }
 
 const AddRecord = () => {
+
+    const handleAddRecord = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const formData = {
+            assignmentID: form.assignmentID.value,
+            employeeID: form.employeeID.value,
+            equipmentID: form.equipmentID.value,
+            assignmentDate: form.assignmentDate.value,
+            date: form.date.value
+        };
+
+        fetch('http://localhost:5000/add_report', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Report Item Add Failed!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                alert('Report Item added successfully!');
+                form.reset();
+            } else {
+                alert(`Report Item add failed: ${data.message}`);
+                form.reset();
+            }
+        })
+        .catch((error) => {
+            alert(`Report Item add failed: ${error.message}`);
+            form.reset();
+        });
+    }
+
     return(
         <div className='DatabaseFormContainer'>
-            <form name='addrecord'>
+            <form name='addrecord' onSubmit={handleAddRecord}>
                 <div>
                     <label>Add Report</label>
                     <input type="text" name="assignmentID" placeholder='Assignment ID' required/>
@@ -89,9 +255,51 @@ const AddRecord = () => {
 }
 
 const UpdateRecord = () => {
+
+    const handleUpdateRecord = (event) => {
+        event.preventDefault();
+
+        const form = event.target;
+
+        const formData = {
+            assignmentID: form.assignmentID.value,
+            employeeID: form.employeeID.value,
+            equipmentID: form.equipmentID.value,
+            assignmentDate: form.assignmentDate.value,
+            returnDate: form.returnDate.value
+        };
+
+        fetch('http://localhost:5000/update_report', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Update Report Failed!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.success) {
+                alert('Report updated successfully!');
+                form.reset();
+            } else {
+                alert(`Report update failed: ${data.message}`);
+                form.reset();
+            }
+        })
+        .catch((error) => {
+            alert(`Report update failed: ${error.message}`);
+            form.reset();
+        });
+    }
+
     return(
         <div className='DatabaseFormContainer'>
-        <form name='updaterecord'>
+        <form name='updaterecord' onSubmit={handleUpdateRecord}>
             <div>
                 <h2>Update Report Item</h2>
                 <label>Assignment ID For Record Update</label>
